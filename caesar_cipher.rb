56e058902e03a353682adc6ec29c1927cd4ff20e
Anna Gladush <ann.gladush@gmail.com>
2026-01-09 17:04:59 +0200

    Finished Caesar decipher, took quite a lot of time by figuring equations. Happy it finally works!

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,12 @@
+# By default step is chosen as 1.
+# caesar_cipher("Zorro!")
+# caesar_cipher("What a string!", 5)
+
+# caesar_decipher("Bmfy f xywnsl!", 5)
+# caesar_decipher("Apssp!")
+
 def caesar_cipher(string, step = 1)
-  # You will need to remember how to convert a string into a number.
-  # Don’t forget to wrap from z to a.
-  # Don’t forget to keep the same case.
-  # The Wikipedia quote discusses a Caesar cipher using a left shift.
-  
+
   str_array = string.split.to_a
   new_words = []
   result = []
@@ -36,12 +39,36 @@ def caesar_cipher(string, step = 1)
   result.join
 end
 
-def caesar_decipher(string, step)
-  # Don’t forget to wrap from z to a.
-  # Don’t forget to keep the same case.
-  
+def caesar_decipher(string, step = 1)
+
+  str_array = string.split.to_a
+  new_words = []
+  result = []
+  str_array.each do |words|
+    deciphered_letter = []
+    word = words.split("")
+    word.each do |letter|
+      if (letter.ord - step) < 65 && (65 <= letter.ord && letter.ord <= 90)
+        letter_shifted =  90 - (65 + (step -1) - letter.ord)
+        deciphered_letter.push((letter_shifted.ord).chr)
+      elsif (letter.ord - step) < 97 && (97 <= letter.ord && letter.ord <= 122)
+        letter_shifted = 97 - (122 + (step -1) - letter.ord)
+        deciphered_letter.push((letter_shifted.ord).chr)
+      elsif (65 <= letter.ord && letter.ord <= 90) || (97 <= letter.ord && letter.ord <= 122)
+        deciphered_letter.push((letter.ord - step).chr) 
+      else
+        deciphered_letter.push(letter)
+      end
+    end
+    new_words.push(deciphered_letter, " ")
+  end
+  new_words.each do |words|
+    if words.respond_to?(:to_a)
+      result.push(words.join)
+    else
+      result.push(words)
+    end
+  end
+  result.pop
+  result.join    
 end
-p caesar_cipher("Zorro!")
-p caesar_cipher("What a string!", 5)
-# caesar_cipher("What a string!", 5)
-# caesar_decipher("Bmfy f xywnsl!", 5)
