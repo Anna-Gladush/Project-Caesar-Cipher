2cc885aace4af4865028237428f25804de5d8d79
Anna Gladush <ann.gladush@gmail.com>
2026-01-21 15:04:49 +0200

    Added a function to pick X or O

--- tic-tac-toe/main.rb
+++ tic-tac-toe/main.rb
@@ -6,9 +6,49 @@ d = '-----------'
 e = ' 7 | 8 | 9 '
 f = '-----------'
 puts a, b, c, d, e, f
+
+used_numbers = []
+3.times do
+  puts 'Your turn: '
+  answer = gets.chomp
+  if a.include?(answer)
+    a = a.gsub(answer, 'X')
+  elsif c.include?(answer)
+    c = c.gsub(answer, 'X')
+  elsif e.include?(answer)
+    e = e.gsub(answer, 'X')
+  end
+  used_numbers << answer
+  puts a, b, c, d, e, f
+end
+p used_numbers
+# puts c.count('O')
 # puts `clear` #will clear the output
-a =  a.gsub('1', 'X')
-c = c.gsub('6', 'O')
-c = c.gsub('4', 'O')
-puts c.count('O')
-puts a, b, c, d, e, f
+
+class Player
+  def turn
+    answer = gets.chomp
+    while answer >= 1 && answer <= 9
+      used_numbers << answer
+    end
+  end
+  def symbol
+    sym = nil
+    until sym == 'X' || sym == 'O'
+      puts 'Pick "X" or "O"'
+      sym = gets.chomp
+      sym.upcase!
+    end
+    sym
+  end
+end
+
+player1 = Player.new
+player2 = Player.new
+choice_of_p1 = player1.symbol
+
+if choice_of_p1 == 'X'
+  choice_of_p2 = 'O'
+else 
+  choice_of_p2 = 'X'
+end
